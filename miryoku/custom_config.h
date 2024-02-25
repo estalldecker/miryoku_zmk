b78ce5e49d5dae4554f72defadcce0ce7cabe6a4
estalldecker <estalldecker@gmail.com>
2024-02-24 23:33:58 -0300

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,4 +1,5 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-//MIRYOKU_ALPHAS=QWERTY
-
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_CLIPBOARD_WIN
